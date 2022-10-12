55309a3c485627d550a996ba24f6ae7cdd3d4698
ErkhanDV <erhan.denis@list.ru>
2022-10-13 03:20:33 +0500

    task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+ const alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+]
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, word) {
+    if (str === undefined || word === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    let newStr = '';
+    for (let i = 0, k = 0; i < str.length; i++, k++) {
+      if (k == word.length) {
+        k = k - word.length;
+      }
+      if (alphabet.indexOf(str[i].toLowerCase()) != -1) {
+        const index = alphabet.indexOf(str[i].toLowerCase()) + alphabet.indexOf(word[k].toLowerCase()) > 25 ?
+          alphabet.indexOf(str[i].toLowerCase()) + alphabet.indexOf(word[k].toLowerCase()) - 26 :
+          alphabet.indexOf(str[i].toLowerCase()) + alphabet.indexOf(word[k].toLowerCase());
+        newStr += alphabet[index];
+      } else {
+        newStr += str[i];
+        k--;
+      }  
+    }
+    return this.type == true ? newStr.toUpperCase() : newStr.toUpperCase().split('').reverse().join('');
+  }
+  decrypt(str, word) {
+    if (str === undefined || word === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    let newStr = '';
+    for (let i = 0, k = 0; i < str.length; i++, k++) {
+      if (k == word.length) {
+        k = k - word.length;
+      }
+      if (alphabet.indexOf(str[i].toLowerCase()) != -1) {
+        const index = alphabet.indexOf(str[i].toLowerCase()) - alphabet.indexOf(word[k].toLowerCase()) < 0 ?
+          alphabet.indexOf(str[i].toLowerCase()) - alphabet.indexOf(word[k].toLowerCase()) + 26 :
+          alphabet.indexOf(str[i].toLowerCase()) - alphabet.indexOf(word[k].toLowerCase());
+        newStr += alphabet[index];
+      } else {
+        newStr += str[i];
+        k--;
+      }  
+    }
+    return this.type == true ? newStr.toUpperCase() : newStr.toUpperCase().split('').reverse().join('');
   }
 }
 
